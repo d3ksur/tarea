e17d71e39f628c8b878e1ad8379f909ca9469617
Franco Reyes <frankkog09@gmail.com>
2023-03-17 21:38:07 -0300

    master auto-commit

---
+++ tarea.py
@@ -0,0 +1,28 @@
+# Esto es del primer trabajo de programacion
+
+print("Hola Mundo")
+print(42)
+print(3.14)
+
+nombre = "Ana"
+ciudad = "Buenos Aires"
+direccion = "Calle Falsa 123"
+
+edad = 25
+cantidad_productos = 10
+dia_mes = 18
+
+precio = 5.99
+temperatura = 25.5
+altura = 1.75
+
+es_mayor_de_edad = True
+es_hombre = False
+
+#Esto es un comentario de una sola línea
+"""
+Esto es un comentario
+de múltiples líneas
+"""
+
+# Esto es del segundo trabajo de programacion
