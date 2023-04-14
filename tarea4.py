93ee676e66500e4ad3e22a7bd5a26f65b0418c8e
Franco Reyes <frankkog09@gmail.com>
2023-04-14 12:03:26 -0300

    master auto-commit

---
+++ tarea4.py
@@ -0,0 +1,26 @@
+#1
+mi_tupla = (1, 2, 3, "cuatro", True)
+
+#2
+mi_tupla = (1, 2, 3)
+print(mi_tupla[2])
+
+#3
+mi_lista = [1, 2, 3, "cuatro", True]
+
+#4
+mi_lista = [1, 2, 3, "cuatro", True]
+print(mi_lista[2])
+
+#5
+variable = [2, "tres", True, ["uno", 10]]
+print(variable[3][1])
+
+#6
+mi_lista = [1, 2, 3, "cuatro", True]
+mi_lista[1] = "dos"
+print(mi_lista)
+
+#7
+mi_rango = range(1, 6)
+print(list(mi_rango))
